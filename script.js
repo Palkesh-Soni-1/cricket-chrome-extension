d348d33bf67575daf53732684796a7a16aaa1be8
Palkesh Soni <sonipalkesh43@gmail.com>
2024-06-25 18:20:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/cricScore?apikey=f19a5046-da30-4db6-a542-d7374b7d30bb")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+            
+            const relevantData = matchesList.filter(match=>match.series=="ICC Mens T20 World Cup 2024").map(match => `${match.t1} vs ${match.t2} \n Score -:  ${match.t1}-: ${match.t1s} , ${match.t2}-: ${match.t2s} \n ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
