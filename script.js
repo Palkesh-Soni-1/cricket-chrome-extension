6d5e4f309fe1186146351a01a1b28dbc2776c95e
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2024-06-26 00:59:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+console.log(1)
 async function getMatchData() {
-
+    console.log(2)
     return await fetch("https://api.cricapi.com/v1/cricScore?apikey=f19a5046-da30-4db6-a542-d7374b7d30bb")
         .then(data => data.json())
         .then(data => {
@@ -9,6 +10,7 @@ async function getMatchData() {
 
             if(!matchesList)return [];
             
+            // const relevantData = matchesList.match.map(match => `${match.name}, ${match.status}`);
             const relevantData = matchesList.filter(match=>match.series=="ICC Mens T20 World Cup 2024").map(match => `${match.t1} vs ${match.t2} \n Score -:  ${match.t1}-: ${match.t1s} , ${match.t2}-: ${match.t2s} \n ${match.status}`);
 
             console.log({relevantData});
@@ -20,4 +22,7 @@ async function getMatchData() {
         })
         .catch(e => console.log(e));
 }
+getMatchData();
+
+
 
