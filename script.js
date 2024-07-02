e6c6203382fa1d8bc9fa0c637900558bb0ae9ddb
Palkesh Soni <sonipalkesh43@gmail.com>
2024-07-02 21:20:15 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.log(1)
+// console.log(1)
 async function getMatchData() {
-    console.log(2)
+    // console.log(2)
     return await fetch("https://api.cricapi.com/v1/cricScore?apikey=f19a5046-da30-4db6-a542-d7374b7d30bb")
         .then(data => data.json())
         .then(data => {
@@ -9,9 +9,10 @@ async function getMatchData() {
             const matchesList = data.data;
 
             if(!matchesList)return [];
-            
             // const relevantData = matchesList.match.map(match => `${match.name}, ${match.status}`);
-            const relevantData = matchesList.filter(match=>match.series=="ICC Mens T20 World Cup 2024").map(match => `${match.t1} vs ${match.t2} \n Score -:  ${match.t1}-: ${match.t1s} , ${match.t2}-: ${match.t2s} \n ${match.status}`);
+            const relevantData = matchesList.filter(match=>match.series=="ICC Mens T20 World Cup 2024"||match.series=="India tour of Zimbabwe, 2024").map(match => `${match.t1} vs ${match.t2} , \n Score -:  ${match.t1}-: ${match.t1s} , ${match.t2}-: ${match.t2s} \n ${match.status} ,  \n Date&Time-:  ${match.dateTimeGMT}`);
+            // console.log(match.series);
+            // document.getElementById("head").innerHTML = `${match.series} ongoing matches`;
 
             console.log({relevantData});
 
